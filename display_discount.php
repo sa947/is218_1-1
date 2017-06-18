6af619c362dd66b2dfb429a3f61f95563e81fbda
sa947 <sa947@njit.edu>
2017-06-17 21:34:42 -0400

    Added php

--- display_discount.php
+++ display_discount.php
@@ -8,56 +8,30 @@
 			    $discount = $list_price * $discount_percent * .01;
 			        $discount_price = $list_price - $discount;
 				    
-				        // apply currency formatting to the
-					dollar and percent amounts
-					    $list_price_formatted =
-					    "$".number_format($list_price, 2);
-					        $discount_percent_formatted =
-						$discount_percent."%";
-						    $discount_formatted =
-						    "$".number_format($discount,
-						    2);
-						        $discount_price_formatted
-							=
-							"$".number_format($discount_price,
-							2);            
+				        // apply currency formatting to the dollar and percent amounts
+					    $list_price_formatted = "$".number_format($list_price, 2);
+					        $discount_percent_formatted = $discount_percent."%";
+						    $discount_formatted = "$".number_format($discount, 2);
+						        $discount_price_formatted = "$".number_format($discount_price, 2);            
 							    
-							        // escape the
-								unformatted
-								input
-								    $product_description_escaped
-								    =
-								    htmlspecialchars($product_description);
+							        // escape the unformatted input
+								    $product_description_escaped = htmlspecialchars($product_description);
 								    ?>
-								    <!DOCTYPE
-								    html>
+								    <!DOCTYPE html>
 								    <html>
 								    <head>
-								        <title>Product
-									Discount
-									Calculator</title>
-									    <link
-									    rel="stylesheet"
-									    type="text/css"
-									    href="main.css">
+								        <title>Product Discount Calculator</title>
+									    <link rel="stylesheet" type="text/css" href="main.css">
 									    </head>
 									    <body>
 									        <main>
-										        <h1>Product
-											Discount
-											Calculator</h1>
-
-											        <label>Product
-												Description:</label>
-												        <span><?php
-													echo
+										        <h1>Product Discount Calculator</h1>
+											        <label>Product Description:</label>
+												        <span><?php echo
 													$product_description_escaped;
 													?></span><br>
-
-													        <label>List
-														Price:</label>
-														        <span><?php
-															echo
+													        <label>List Price:</label>
+														        <span><?php echo
 															$list_price_formatted;
 															?></span><br>
 
@@ -68,6 +42,7 @@
 																	$discount_percent_formatted;
 																	?></span><br>
 
+
 																	        <label>Discount
 																		Amount:</label>
 																		        <span><?php
@@ -75,12 +50,14 @@
 																			$discount_formatted;
 																			?></span><br>
 
+
 																			        <label>Discount
 																				Price:</label>
 																				        <span><?php
 																					echo
 																					$discount_price_formatted;
 																					?></span><br>
+
 																					    </main>
 																					    </body>
 																					    </html>
